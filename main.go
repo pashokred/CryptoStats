24714587d17fa24a6424f0bbde229719b4a8d2a3
Pashok <pavlikredko@live.com>
2022-08-01 11:45:52 +0300

    Working app, fixed server

--- main.go
+++ main.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 var (
-	gMarket string = "btcuah"
+	gMarket = "btcuah"
 )
-
-type emailJSON struct {
-	emails []string
-}
 
 func main() {
 
-	http.HandleFunc("/rate", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/rate", func(w http.ResponseWriter, r *http.Request) {
 		price, err := GetLatestPrice(gMarket)
 		if err != nil {
 			log.Fatal(err)
@@ -27,28 +20,44 @@ func main() {
 
 		w.WriteHeader(http.StatusOK)
 		resp := make(map[string]string)
-		priceStr := strconv.FormatFloat(price, 'E', -1, 64)
+		priceStr := float64ToString(price)
 		resp["mktprice"] = priceStr
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 		}
-
-		saveToFile(priceStr)
 		w.Write(jsonResp)
 	})
 
-	http.HandleFunc("/subscribe", func(rw http.ResponseWriter, r *http.Request) {
-		reqBody, err := ioutil.ReadAll(r.Body)
+	http.HandleFunc("/api/subscribe", func(w http.ResponseWriter, r *http.Request) {
+		headerContentType := r.Header.Get("Content-Type")
+		if headerContentType != "application/x-www-form-urlencoded" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			return
+		}
+		r.ParseForm()
+		var data []string
+		for _, value := range r.Form {
+			data = append(data, value[0])
+		}
+
+		if emailExist, err := saveToFile(data[0]); err != nil {
+			log.Fatal(err)
+		} else if emailExist {
+			w.WriteHeader(http.StatusConflict)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+	})
+
+	http.HandleFunc("/api/sendEmails", func(rw http.ResponseWriter, r *http.Request) {
+		price, err := GetLatestPrice(gMarket)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%s\n", reqBody)
-		w.Write([]byte("Received a POST request\n"))
-	})
-
-	http.HandleFunc("/sendEmails", func(rw http.ResponseWriter, r *http.Request) {
-		// Get data from json, send to all emails using net/http
+		if err = sendEmails(price); err != nil {
+			log.Fatal(err)
+		}
 	})
 
 	http.ListenAndServe(":8000", nil)
