ff315a8836066b4b2ee2a7a7e3ecbfdeddd6c800
Pashok <pavlikredko@live.com>
2022-08-01 11:45:52 +0300

    Working app, fixed server

--- json.go
+++ json.go
@@ -3,11 +3,61 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/mail"
+	"net/smtp"
 	"os"
+	"strconv"
 )
 
-type MailStruct struct {
-	Emails string `json:"emails"`
+var filename = "emails.json"
+
+func saveToFile(emailToSave string) (emailExist bool, err error) {
+	data, err := getDataFromJSON()
+	if err != nil {
+		return false, err
+	}
+
+	// Check if email exist in json database
+	for _, email := range data {
+		if email == emailToSave {
+			return true, err
+		}
+	}
+
+	// Check if email valid
+	if _, err := mail.ParseAddress(emailToSave); err != nil {
+		return false, err
+	}
+
+	data = append(data, emailToSave)
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return false, err
+	}
+
+	err = ioutil.WriteFile(filename, dataBytes, 0644)
+	if err != nil {
+		return false, err
+	}
+	return false, err
+}
+
+func getDataFromJSON() (data []string, err error) {
+	err = checkFile(filename)
+	if err != nil {
+		return data, err
+	}
+
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return data, err
+	}
+	if len(file) == 0 {
+		return []string{}, nil
+	}
+
+	err = json.Unmarshal(file, &data)
+	return data, err
 }
 
 func checkFile(filename string) error {
@@ -21,35 +71,27 @@ func checkFile(filename string) error {
 	return nil
 }
 
-func saveToFile(dataToSave string) error {
-	filename := "emails.json"
-	err := checkFile(filename)
+func sendEmails(price float64) (err error) {
+	data, err := getDataFromJSON()
 	if err != nil {
 		return err
 	}
+	return sendCurrentPrice(price, data)
+}
 
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return err
-	}
+func float64ToString(f float64) string {
+	return strconv.FormatFloat(f, 'G', -1, 64)
+}
 
-	data := []MailStruct{}
+func sendCurrentPrice(price float64, emails []string) error {
+	from := "redkopavli@gmail.com"
+	password := "kgshbnijjpcwmaji"
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
 
-	// Make loop and check if exist element
-
-	json.Unmarshal(file, &data)
-
-	newStruct := &MailStruct{
-		Emails: dataToSave,
-	}
-
-	data = append(data, *newStruct)
-	dataBytes, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(filename, dataBytes, 0644)
+	message := []byte(float64ToString(price))
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, emails, message)
 	if err != nil {
 		return err
 	}
